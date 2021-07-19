eab01314113c4bcecfcd1c82a96a38f2739bcff7
MinseokCHAE <minseok6739@naver.com>
2021-07-19 17:42:58 +0900

    update_0719_17:42

--- study/keras/keras32_1_mnist_dnn.py
+++ study/keras/keras32_1_mnist_dnn.py
@@ -1,21 +1,22 @@
+import time
 import numpy as np
-from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, QuantileTransformer, OneHotEncoder
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import Dense, Input, Conv2D, Flatten, MaxPooling2D
+from tensorflow.keras.layers import Dense, Input, Conv2D, Flatten, MaxPooling2D, GlobalAvgPool2D, Dropout
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.utils import to_categorical
 
 
 #1. data preprocessing
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = x_train.reshape((60000, 28, 28, 1))
-x_test = x_test.reshape((10000, 28, 28, 1))
-# 차원 1up, data요소/순서 바뀌면안됨, flatten한값 동일 -> ex) (60000, 28, 14, 2) 가능
-x_train, x_test = x_train / 255, x_test / 255
-#scaler는 2차원이하만 적용가능, 최대값이 255이므로 수동 MinMaxScaling
+x_train = x_train.reshape((60000, 28 * 28 * 1))
+x_test = x_test.reshape((10000, 28 * 28 * 1))
+scaler = MinMaxScaler()
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
 
 # print(np.unique(y_train)) # [0 1 2 3 4 5 6 7 8 9]
 y_train = to_categorical(y_train)
@@ -28,15 +29,13 @@ y_test = to_categorical(y_test)
 # y_test = onehot_encoder.fit_transform(y_test).toarray()
 
 #2. modeling
-input = Input(shape=(28, 28, 1))
-x = Conv2D(64, (3,3), activation='relu')(input)
-x = MaxPooling2D((2,2))(x)
-x = Conv2D(64, (3,3), activation='relu')(x)
-x = MaxPooling2D((2,2))(x)
-x = Conv2D(32, (3,3), activation='relu')(x)
-x = Flatten()(x)
+input = Input(shape=(28 * 28 * 1, ))
+x = Dense(32, activation='relu')(input)
+x = Dropout(0.2)(x)
 x = Dense(64, activation='relu')(x)
+x = Dropout(0.2)(x)
 x = Dense(32, activation='relu')(x)
+x = Dropout(0.2)(x)
 output = Dense(10, activation='softmax')(x)
 
 model = Model(inputs=input, outputs=output)
@@ -44,17 +43,45 @@ model = Model(inputs=input, outputs=output)
 #3. compiling, training
 es = EarlyStopping(monitor='val_loss', patience=10, mode='min', verbose=1)
 model.compile(loss='categorical_crossentropy', optimizer='adam', 
-                        metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=39, batch_size=512, 
+                        metrics=['acc'])
+start_time = time.time()
+hist = model.fit(x_train, y_train, epochs=64, batch_size=32, 
                                 validation_split=0.001, callbacks=[es])
+end_time = time.time() - start_time
 
 #4. evaluating, prediction
 loss = model.evaluate(x_test, y_test)
 
 print('loss = ', loss[0])
 print('accuracy = ', loss[1])
+print('time taken(s) : ', end_time)
+
+#5. plt visualization
+import matplotlib.pyplot as plt
+plt.figure(figsize=(9,5))
+
+plt.subplot(2,1,1)
+plt.plot(hist.history['loss'], marker='.', c='red', label='loss')
+plt.plot(hist.history['val_loss'], marker='.', c='blue', label='val_loss')
+plt.grid()
+plt.title('loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(loc='upper right')
+
+plt.subplot(2,1,2)
+plt.plot(hist.history['acc'])
+plt.plot(hist.history['val_acc'])
+plt.grid()
+plt.title('acc')
+plt.ylabel('acc')
+plt.xlabel('epoch')
+plt.legend(['acc', 'val_acc'])
+
+plt.show()
 
 '''
-loss =  0.03041081316769123
-accuracy =  0.9926000237464905
+loss =  0.1202073022723198
+accuracy =  0.9659000039100647
+time taken(s) :  88.60119223594666
 '''
