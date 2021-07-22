f30f67c457f0bde1f600a59f394304700ec26258
MinseokCHAE <minseok6739@naver.com>
2021-07-22 16:20:48 +0900

    update_0722_16:20

--- study/samsung/CMS_1_123456.py
+++ study/samsung/CMS_1_123456.py
@@ -20,52 +20,100 @@ datasets_sk = datasets_sk.dropna(axis=0)
 # print(datasets_samsung.head())
 # print(datasets_sk.head())
 
-x1 = datasets_samsung.iloc[:, [0,1,2,4]]
-y1 = datasets_samsung.iloc[:, 3]
-# print(x1.head(), y1.head())
-# print(x1.shape, y1.shape) #(2602, 4) (2602,)
-x2 = datasets_sk.iloc[:, [0,1,2,4]]
-y2 = datasets_sk.iloc[:, 3]
-# print(x2.head(), y2.head())
-# print(x2.shape, y2.shape) #(2602, 4) (2602,)
+#가격과 거래량 수치차이가 크기때문에 따로 MinMaxScaling
+data1_ss = datasets_samsung.iloc[:, :-1]
+data2_ss = datasets_samsung.iloc[:, -1:]
+# print(data1_ss.head(), data2_ss.head())
+scaler = MinMaxScaler()
+scaler.fit(data1_ss)
+data1_ss = scaler.transform(data1_ss)
+scaler.fit(data2_ss)
+data2_ss = scaler.transform(data2_ss)
+# print(data1_ss, data2_ss)
+data_ss = np.concatenate((data1_ss, data2_ss), axis=1)
+# print(data_ss.shape) # (2602, 5)
 
-x1_train, x1_test, x2_train, x2_test, y1_train, y1_test, y2_train, y2_test = train_test_split(x1, x2, y1, y2, test_size=0.1, random_state=9)
-scaler = StandardScaler()
-scaler.fit(x1_train)
-x1_train = scaler.transform(x1_train)
-x1_test = scaler.transform(x1_test)
-x2_train = scaler.transform(x2_train)
-x2_test = scaler.transform(x2_test)
+data1_sk = datasets_sk.iloc[:, :-1]
+data2_sk = datasets_sk.iloc[:, -1:]
+# print(data1_sk.head(), data2_sk.head())
+scaler = MinMaxScaler()
+scaler.fit(data1_sk)
+data1_sk = scaler.transform(data1_sk)
+scaler.fit(data2_sk)
+data2_sk = scaler.transform(data2_sk)
+# print(data1_sk, data2_sk)
+data_sk = np.concatenate((data1_sk, data2_sk), axis=1)
+# print(data_sk.shape) # (2602, 5)
+'''
+************************Change Here************************
+'''
+target = data_ss[:, [-2]] # 첫날 target =  '삼성' + '종가' = data_ss + 뒤에서 2번째 열 
+# print(target.shape) # (2602, 1)
+'''
+************************Change Here************************
+'''
+
+x1 = []
+x2 = []
+y = []
+size = 50 # 몇일단위로 자를것인지 설정
+for i in range(len(target) - (size + 1)):
+    x1.append(data_ss[i: i + (size + 1)])
+    y.append(target[i + (size + 1)])
+for i in range(len(target) - (size + 1)):
+    x2.append(data_sk[i: i + (size + 1)])
+x1_pred = [data_ss[len(data_ss) - ( size + 1): ]]
+x2_pred = [data_sk[len(data_ss) - ( size + 1): ]]
+
+x1 = np.array(x1)
+x2 = np.array(x2)
+y = np.array(y)
+x1_pred = np.array(x1_pred)
+x2_pred = np.array(x2_pred)
+# print(x1.shape, x2.shape, y.shape, x1_pred.shape, x2_pred.shape) # (2551, 51, 5) (2551, 51, 5) (2551, 1) (1, 51, 5) (1, 51, 5)
+
+x1_train, x1_test, x2_train, x2_test, y_train, y_test = train_test_split(x1, x2, y, test_size=0.2, random_state=9)
 
 #2. Modeling
-input1 = Input(shape=(4, ))
-s1 = Dense(10, activation='relu')(input1)
-s1 = Dense(10, activation='relu')(s1)
+input1 = Input(shape=(51, 5))
+d1 = LSTM(16, activation='relu')(input1)
 
-input2 = Input(shape=(4, ))
-s2 = Dense(10, activation='relu')(input2)
-s2 = Dense(10, activation='relu')(s2)
+input2 = Input(shape=(51, 5))
+d2 = LSTM(16, activation='relu')(input2)
 
 from tensorflow.keras.layers import concatenate
-merge = concatenate([s1, s2])
-m = Dense(10, activation='relu')(merge)
-m = Dense(10, activation='relu')(m)
+m = concatenate([d1, d2])
 output = Dense(1, activation='relu')(m)
 
 model = Model(inputs=[input1, input2], outputs=output)
 
 #3. Compiling, Training
-es = EarlyStopping(monitor='val_loss', patience=32, mode='min', verbose=1)
 model.compile(loss='mse', optimizer='adam')
-model.fit([x1_train, x2_train], y1_train, epochs=100, batch_size=8, verbose=1)
+
+date = datetime.datetime.now()
+date_time = date.strftime('%m%d_%H%M')
+path = './_save/MCP/'
+info = '{epoch:04d}_{val_loss:.4f}'
+filepath = ''.join([path, 'test', '_', date_time, '_', info, '.hdf5'])
+cp = ModelCheckpoint(monitor='val_loss', save_best_only=True, mode='auto', verbose=1, filepath=filepath)
+es = EarlyStopping(monitor='val_loss', restore_best_weights=False, mode='auto', verbose=1, patience=4)
+
+start_time = time.time()
+model.fit([x1_train, x2_train], y_train, epochs=8, batch_size=16, verbose=1, validation_split=0.001, callbacks=[es, cp])
+end_time = time.time() - start_time
 
 #4. Evaluating, Prediction
-loss = model.evaluate([x1_test, x2_test], y1_test)
-y1_predict = model.predict([x1_test, x2_test])
-r2 = r2_score(y1_test, y1_predict)
+loss = model.evaluate([x1_test, x2_test], y_test)
+y_pred = model.predict([x1_pred, x2_pred])
+y_pred = scaler.inverse_transform(y_pred)
 
 print('loss = ', loss)
-print('r2 score = ', r2)
+print("Tomorrow's stock price = ", y_pred)
+print('time taken(s) : ', end_time)
+
+'''
+
+'''
 
 #5. Plt Visualization
 '''
