f46110820d4e36760049e386f6f79af69af80bb9
MinseokCHAE <minseok6739@naver.com>
2021-07-22 18:12:08 +0900

    update_0722_18:11

--- study/samsung/CMS1_123456.py
+++ study/samsung/CMS1_123456.py
@@ -12,17 +12,19 @@ from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
 
 #1. Data Preprocessing
-datasets_samsung = pd.read_csv('../_data/삼성전자 주가 20210721.csv', header=0, usecols=[1,2,3,4,10], nrows=2602, encoding='EUC-KR')
+# read_csv
 datasets_sk = pd.read_csv('../_data/SK주가 20210721.csv', header=0, usecols=[1,2,3,4,10], nrows=2602, encoding='EUC-KR')
+datasets_samsung = pd.read_csv('../_data/삼성전자 주가 20210721.csv', header=0, usecols=[1,2,3,4,10], nrows=2602, encoding='EUC-KR')
 
-datasets_samsung = datasets_samsung.dropna(axis=0)
+# null값 제거
 datasets_sk = datasets_sk.dropna(axis=0)
-# print(datasets_samsung.head())
+datasets_samsung = datasets_samsung.dropna(axis=0)
 # print(datasets_sk.head())
+# print(datasets_samsung.head())
 
-#가격과 거래량 수치차이가 크기때문에 따로 MinMaxScaling
-data1_ss = datasets_samsung.iloc[:, :-1]
-data2_ss = datasets_samsung.iloc[:, -1:]
+# 주가4종류 (시가,고가,저가,종가) 와 거래량의 수치차이가 크기때문에 따로 MinMaxScaling - samsung
+data1_ss = datasets_samsung.iloc[:, :-1] # 주가4종 추출
+data2_ss = datasets_samsung.iloc[:, -1:] # 거래량 추출
 # print(data1_ss.head(), data2_ss.head())
 scaler = MinMaxScaler()
 scaler.fit(data1_ss)
@@ -30,11 +32,12 @@ data1_ss = scaler.transform(data1_ss)
 scaler.fit(data2_ss)
 data2_ss = scaler.transform(data2_ss)
 # print(data1_ss, data2_ss)
-data_ss = np.concatenate((data1_ss, data2_ss), axis=1)
+data_ss = np.concatenate((data1_ss, data2_ss), axis=1) # 병합 (주가4종 오른쪽 열에 거래량 추가)
 # print(data_ss.shape) # (2602, 5)
 
-data1_sk = datasets_sk.iloc[:, :-1]
-data2_sk = datasets_sk.iloc[:, -1:]
+# 상동 - sk
+data1_sk = datasets_sk.iloc[:, :-1] 
+data2_sk = datasets_sk.iloc[:, -1:] 
 # print(data1_sk.head(), data2_sk.head())
 scaler = MinMaxScaler()
 scaler.fit(data1_sk)
@@ -42,7 +45,7 @@ data1_sk = scaler.transform(data1_sk)
 scaler.fit(data2_sk)
 data2_sk = scaler.transform(data2_sk)
 # print(data1_sk, data2_sk)
-data_sk = np.concatenate((data1_sk, data2_sk), axis=1)
+data_sk = np.concatenate((data1_sk, data2_sk), axis=1) 
 # print(data_sk.shape) # (2602, 5)
 '''
 ************************Change Here************************
@@ -53,32 +56,34 @@ target = data_ss[:, [-2]] # 첫날 target =  '삼성' + '종가' = data_ss + 뒤
 ************************Change Here************************
 '''
 
-x1 = []
-x2 = []
-y = []
-size = 50 # 몇일단위로 자를것인지 설정
-for i in range(len(target) - (size + 1)):
-    x1.append(data_ss[i: i + (size + 1)])
-    y.append(target[i + (size + 1)])
-for i in range(len(target) - (size + 1)):
-    x2.append(data_sk[i: i + (size + 1)])
-x1_pred = [data_ss[len(data_ss) - ( size + 1): ]]
-x2_pred = [data_sk[len(data_ss) - ( size + 1): ]]
+# LSTM 처리를 위한 data_split (= split_x)
+x1 = [] # 삼성
+x2 = [] # SK
+y = [] # target
+size = 50 # 데이터 slice 단위일수 설정 (단위일수만큼 끊어서 저장) -> 단위일수만큼의 데이터를 가지고 그 다음날의 주가 예측
+for i in range(len(target) - size + 1):
+    x1.append(data_ss[i: (i + size) ])
+    x2.append(data_sk[i: (i + size) ])
+    y.append(target[i + (size - 1)]) 
+# 설정한 단위일수 만큼 최근 데이터 slice -> y_predict 를 위한 x1_pred, x2_pred 생성
+x1_pred = [data_ss[len(data_ss) - size : ]]
+x2_pred = [data_sk[len(data_ss) - size : ]]
 
+# numpy 배열화
 x1 = np.array(x1)
 x2 = np.array(x2)
 y = np.array(y)
 x1_pred = np.array(x1_pred)
 x2_pred = np.array(x2_pred)
-# print(x1.shape, x2.shape, y.shape, x1_pred.shape, x2_pred.shape) # (2551, 51, 5) (2551, 51, 5) (2551, 1) (1, 51, 5) (1, 51, 5)
+print(x1.shape, x2.shape, y.shape, x1_pred.shape, x2_pred.shape) # (2553, 50, 5) (2553, 50, 5) (2553, 1) (1, 50, 5) (1, 50, 5)
 
-x1_train, x1_test, x2_train, x2_test, y_train, y_test = train_test_split(x1, x2, y, test_size=0.2, random_state=9)
+x1_train, x1_test, x2_train, x2_test, y_train, y_test = train_test_split(x1, x2, y, test_size=0.2, random_state=66)
 
 #2. Modeling
-input1 = Input(shape=(51, 5))
+input1 = Input(shape=(50, 5))
 d1 = LSTM(16, activation='relu')(input1)
 
-input2 = Input(shape=(51, 5))
+input2 = Input(shape=(50, 5))
 d2 = LSTM(16, activation='relu')(input2)
 
 from tensorflow.keras.layers import concatenate
@@ -93,50 +98,26 @@ model.compile(loss='mse', optimizer='adam')
 date = datetime.datetime.now()
 date_time = date.strftime('%m%d_%H%M')
 path = './_save/MCP/'
-info = '{epoch:04d}_{val_loss:.4f}'
-filepath = ''.join([path, 'test', '_', date_time, '_', info, '.hdf5'])
+info = '{epoch:02d}_{val_loss:.4f}'
+filepath = ''.join([path, 'CMS1_12345', '_', date_time, '_', info, '.hdf5'])
 cp = ModelCheckpoint(monitor='val_loss', save_best_only=True, mode='auto', verbose=1, filepath=filepath)
 es = EarlyStopping(monitor='val_loss', restore_best_weights=False, mode='auto', verbose=1, patience=4)
 
 start_time = time.time()
-model.fit([x1_train, x2_train], y_train, epochs=8, batch_size=16, verbose=1, validation_split=0.001, callbacks=[es, cp])
+model.fit([x1_train, x2_train], y_train, epochs=4, batch_size=16, verbose=1, validation_split=0.001, callbacks=[es, cp])
 end_time = time.time() - start_time
 
 #4. Evaluating, Prediction
 loss = model.evaluate([x1_test, x2_test], y_test)
 y_pred = model.predict([x1_pred, x2_pred])
-y_pred = scaler.inverse_transform(y_pred)
+y_pred = scaler.inverse_transform(y_pred) # 가격(원) 확인을 위한 inverse scaling
 
 print('loss = ', loss)
-print("Tomorrow's stock price = ", y_pred)
+print("Tomorrow's closing price = ", y_pred)
 print('time taken(s) : ', end_time)
 
 '''
-
-'''
-
-#5. Plt Visualization
-'''
-import matplotlib.pyplot as plt
-plt.figure(figsize=(9,5))
-
-plt.subplot(2,1,1)
-plt.plot(hist.history['loss'], marker='.', c='red', label='loss')
-plt.plot(hist.history['val_loss'], marker='.', c='blue', label='val_loss')
-plt.grid()
-plt.title('loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(loc='upper right')
-
-plt.subplot(2,1,2)
-plt.plot(hist.history['acc'])
-plt.plot(hist.history['val_acc'])
-plt.grid()
-plt.title('acc')
-plt.ylabel('acc')
-plt.xlabel('epoch')
-plt.legend(['acc', 'val_acc'])
-
-plt.show()
+loss =  0.13289673626422882
+Tomorrow's closing price =  [[842562.]]
+time taken(s) :  50.183152198791504
 '''
